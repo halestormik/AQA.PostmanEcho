9c85e385100dbf023526431b11bc360e276e7d24
NetoIQA75 <all-slavin@yandex.ru>
2024-02-04 11:38:41 +0300

    Successful test again

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -18,7 +18,7 @@ public class PostmanEchoTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Phone number: +7-812-556-78-96"))
+                .body("data", equalTo("Phone number: +1-812-556-78-96"))
         ;
     }
 
